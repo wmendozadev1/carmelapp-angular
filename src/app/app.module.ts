5c71d31b52dbd4fbd6f4ed909dc98e7353532d9a
Winston Mendoza G <wjmendozag87@gmail.com>
2021-10-18 12:01:58 -0600

    creando componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MembersComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
